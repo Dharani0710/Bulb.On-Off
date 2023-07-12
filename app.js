99eae0be577a9cbc7004e857b7b51271e64e6e26
Dharani0710 <128206031+Dharani0710@users.noreply.github.com>
2023-07-12 11:03:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on() {
+img.src="./on.jpg";
+}
+function bulb_off(){
+img.src="./off.jpg";
+}
